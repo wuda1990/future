0475e6f3693c7dfc553a6cab5d54f0ccd186e751
huajun <huajun@zhoudongdeMacBook-Air.local>
2018-12-24 21:33:13 +0800

    Initial commit

---
+++ src/main/java/com/quantumn/future/FutureApplication.java
@@ -0,0 +1,14 @@
+package com.quantumn.future;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutureApplication.class, args);
+    }
+
+}
+
