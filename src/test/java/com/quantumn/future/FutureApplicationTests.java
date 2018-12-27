df6953a318fa1d3220cf7e4605f4b29c8ed117a1
stark <753415348@qq.com>
2018-12-27 18:38:16 +0800

    update

--- src/test/java/com/quantumn/future/FutureApplicationTests.java
+++ src/test/java/com/quantumn/future/FutureApplicationTests.java
@@ -26,15 +26,27 @@ public class FutureApplicationTests {
     }
 
     @Test
+    public void simpleExecuteRule() {
+        TradeAuditBo tradeAuditBo = new TradeAuditBo();
+        tradeAuditBo.setPast24hSuccTradeCnt(6);
+        ruleAuditor.audit(tradeAuditBo);
+    }
+
+    @Test
     public void executeRule() {
         Trade trade = new Trade();
         trade.setId(101L);
         trade.setCard("622021001131680503");
         trade.setAmount(2000L);
         trade.setTradeTime(new Date());
-        TradeAuditBo tradeAuditBo = new TradeAuditBo();
-        tradeAuditBo.setPast24hSuccTradeCnt(6);
-        ruleAuditor.audit(tradeAuditBo);
+        ruleAuditor.audit(trade);
+        Trade trade1 = new Trade();
+        trade1.setId(102L);
+        trade1.setCard("622021001131680505");
+        trade1.setAmount(1000L);
+        trade1.setTradeTime(new Date());
+        ruleAuditor.audit(trade1);
+//        ruleAuditor.audit(trade);
     }
 
 }
