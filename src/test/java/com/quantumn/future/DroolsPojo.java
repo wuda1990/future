b194a70e2ec078957eea6e699ad7725f57210516
stark <753415348@qq.com>
2019-01-07 15:39:32 +0800

    update container factory

---
+++ src/test/java/com/quantumn/future/DroolsPojo.java
@@ -0,0 +1,18 @@
+package com.quantumn.future;
+
+import java.util.List;
+
+public class DroolsPojo {
+
+        List<String> answers;
+        //getters and setters
+
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
+    }
+}
