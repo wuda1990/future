140b3100fc9a62f01cf51e79bc5a246b595530e3
huajun.wu <huajun.wu@99bill.com>
2019-07-02 18:06:05 +0800

    add lock

--- src/main/java/com/quantumn/future/async/ReadWriteLock.java
+++ src/main/java/com/quantumn/future/async/ReadWriteLock.java
@@ -1,32 +1,86 @@
 package com.quantumn.future.async;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ReadWriteLock {
-    private int readers;
-    private int writers;
+
+    private Set<Thread> readersSet = new HashSet<>(10);
+    private ThreadLocal<Integer> reentrantReadCnt = new ThreadLocal<>();
+    private Thread writeThread;
     private int writeRequests;
+    private int writeAccess;
 
     public synchronized void lockRead() throws InterruptedException {
-        if (writers > 0 || writeRequests > 0) {
+        Thread currentThread = Thread.currentThread();
+        while (!isGrantReadAccess(currentThread)) {
             wait();
         }
-        readers++;
+        if (reentrantReadCnt.get() == null) {
+            reentrantReadCnt.set(1);
+        }else{
+            reentrantReadCnt.set(reentrantReadCnt.get()+1);
+        }
+        readersSet.add(currentThread);
+    }
+
+    private boolean isGrantReadAccess(Thread callingThread) {
+        if (callingThread == writeThread) return true;
+        if (writeThread!=null)  return false;
+        if (readersSet.contains(callingThread)) return true;
+        if (writeRequests>0) return false;
+        return true;
     }
 
     public synchronized void unlockRead() {
-        readers--;
+        Thread callingThread = Thread.currentThread();
+        if(!readersSet.contains(callingThread)){
+            throw new IllegalMonitorStateException(
+                    "Calling Thread does not hold a read lock on this ReadWriteLock");
+        }
+        Integer cnt = reentrantReadCnt.get();
+        if (cnt == 1) {
+            readersSet.remove(Thread.currentThread());
+        } else {
+            cnt--;
+            reentrantReadCnt.set(cnt);
+        }
         notifyAll();
+
     }
 
     public synchronized void lockWrite() throws InterruptedException {
         writeRequests++;
-        if (readers > 0 || writers > 0) {
+        Thread currentThread = Thread.currentThread();
+        while (!isGrantWriteAccess(currentThread)) {
             wait();
         }
-        writers++;
+        writeRequests--;
+        writeAccess++;
+        writeThread = currentThread;
+    }
+
+    private boolean isGrantWriteAccess(Thread callingThread) {
+        if (writeThread == callingThread) {
+            return true;
+        }
+        if (readersSet.size() == 1 && readersSet.contains(callingThread)) {
+            return true;
+        }
+        if (!readersSet.isEmpty()) {
+            return false;
+        }
+        if (writeThread != null) {
+            return false;
+        }
+        return true;
     }
 
     public synchronized void unlockWrite() {
-        writers--;
+        writeAccess--;
+        if (writeAccess == 0) {
+            writeThread = null;
+        }
         notifyAll();
     }
 }
--- src/test/java/com/quantumn/future/async/LockTest.java
+++ src/test/java/com/quantumn/future/async/LockTest.java
@@ -1,50 +1,58 @@
 package com.quantumn.future.async;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
 
+@Slf4j
 public class LockTest {
     Integer pageCount = 0;
     @Test
     public void testReadWriteLock() throws ExecutionException, InterruptedException {
         ReadWriteLock lock = new ReadWriteLock();
+        CountDownLatch latch = new CountDownLatch(2);
         ExecutorService executorService = Executors.newFixedThreadPool(3);
-        Callable<Integer> reader = new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
+        Callable<Integer> reader = () -> {
+            try {
                 lock.lockRead();
-                System.out.println("reader "+Thread.currentThread().getName()+" is running");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                log.info("reader "+Thread.currentThread().getName()+" is running");
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
                 lock.unlockRead();
-                return pageCount;
+                log.info("reader "+Thread.currentThread().getName()+" is done");
             }
+            return pageCount;
         };
 
-        Callable<Integer> writer = new Callable<Integer>() {
-            @Override
-            public Integer call() throws Exception {
+        Callable<Integer> writer = () -> {
+            try {
                 lock.lockWrite();
-                System.out.println("writer "+Thread.currentThread().getName()+" is running");
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                lock.lockWrite();
+                log.info("writer "+Thread.currentThread().getName()+" is running");
+                Thread.sleep(3000);
                 pageCount++;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
                 lock.unlockWrite();
-                return pageCount;
+                lock.unlockWrite();
+                latch.countDown();
             }
+            return pageCount;
         };
         Future<Integer> f = executorService.submit(reader);
-        System.out.println("page count is :"+ f.get());
+        Thread.sleep(1000);
         executorService.submit(writer);
+        executorService.submit(writer);
+        latch.await();
         Future<Integer> f1 = executorService.submit(reader);
-        System.out.println("page count is :"+ f1.get());
+        log.info("page count is :"+ f1.get());
+        Future<Integer> f2 = executorService.submit(reader);
+        log.info("page count is :"+ f2.get());
     }
 
 }
