81ebfc544da561b21d4b64847f2e6a3326e0db61
huajun.wu <huajun.wu@99bill.com>
2019-09-25 11:04:43 +0800

    add Subject 19

---
+++ src/main/java/com/quantumn/future/algorithm/leetcode/ListNode.java
@@ -0,0 +1,23 @@
+package com.quantumn.future.algorithm.leetcode;
+/**
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+
+    //append the list node ,return the next node
+    public ListNode append(ListNode p) {
+        this.next = p;
+        return p;
+    }
+    public void print() {
+        System.out.print(val);
+        ListNode p = next;
+        while (p != null) {
+            System.out.print("->"+p.val);
+            p = p.next;
+        }
+    }
+}
---
+++ src/main/java/com/quantumn/future/algorithm/leetcode/Subject_19.java
@@ -0,0 +1,115 @@
+package com.quantumn.future.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * 示例：
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 说明：
+ * 给定的 n 保证是有效的。
+ * 进阶：
+ * 你能尝试使用一趟扫描实现吗？
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Subject_19 {
+    //扫描两次
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode p = head;
+        int i=0;//i为链表大小
+        while (p != null) {
+            i++;
+            p = p.next;
+        }
+        int j=0;
+        //如果删除的是头结点
+        if (i == n) {
+            ListNode new_head = head.next;
+            head.next = null;
+            return new_head;
+        }
+        p = head;
+        ListNode pre = p;
+        while (p != null) {
+            if (j++ == i - n) {
+                //remove the node
+                pre.next = p.next;
+                p.next = null;
+                return head;
+            }
+            pre = p;
+            p = p.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Subject_19 demo = new Subject_19();
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.append(node2).append(node3).append(node4).append(node5);
+        demo.removeNthFromEnd3(node1, 3).print();
+    }
+
+    /**
+     * 循环，用map记住位置和node的对应关系
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        Map<Integer, ListNode> map = new HashMap<>();
+        ListNode p = head;
+        int i=0;
+        while (p != null) {
+            map.put(i++, p);
+            p = p.next;
+        }
+        //如果删除的是头结点
+        if (i == n) {
+            ListNode new_head = head.next;
+            head.next = null;
+            return new_head;
+        }
+        //删除中部结点
+        ListNode last_n = map.get(i - n);
+        map.get(i-n-1).next = last_n.next;
+        last_n.next = null;
+        return head;
+    }
+
+    /**
+     * 双指针题解（双指针的用处是真多啊！）
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd3(ListNode head, int n) {
+        //哑指针处理删除头结点或链表只有一个结点的情况
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode start = dummy;
+        ListNode end = dummy;
+        for (int i = 0; i < n; i++) {
+            end = end.next;
+        }
+        while (end.next != null) {
+            start = start.next;
+            end = end.next;
+        }
+        //将前结点start的指针指向要删除结点的下一个结点
+        //将被删除结点的指针指向null
+        ListNode p = start.next;
+        start.next = p.next;
+        p.next = null;
+        return dummy.next;
+    }
+
+}
