5751c12108f3fb967eae8bb7eb8c66d281884700
hua <753415348@qq.com>
2019-09-26 22:38:22 +0800

    add leetCode Subject_20

---
+++ src/main/java/com/quantumn/future/algorithm/leetcode/Subject_20.java
@@ -0,0 +1,77 @@
+package com.quantumn.future.algorithm.leetcode;
+
+import java.util.LinkedList;
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ *     左括号必须用相同类型的右括号闭合。
+ *     左括号必须以正确的顺序闭合。
+ *
+ * 注意空字符串可被认为是有效字符串。
+ *输入: "()[]{}"
+ * 输出: true
+ * 输入: "([)]"
+ * 输出: false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Subject_20 {
+    public boolean isValid(String s) {
+
+        LinkedList<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char value = s.charAt(i);
+            if ('(' == value || '{' == value || '[' == value) {
+                stack.push(value);
+                continue;
+            }
+            if (')' == value && !stack.isEmpty() && '(' == stack.peek()) {
+                stack.pop();
+                continue;
+            }
+            if ('}' == value && !stack.isEmpty() && '{' == stack.peek()) {
+                stack.pop();
+                continue;
+            }
+            if (']' == value && !stack.isEmpty() && '[' == stack.peek()) {
+                stack.pop();
+                continue;
+            }
+            return false;
+        }
+        return stack.isEmpty();
+    }
+
+    public boolean isValid2(String s) {
+        LinkedList<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char value = s.charAt(i);
+            if ('(' == value || '{' == value || '[' == value) {
+                stack.push(value);
+                continue;
+            }
+            char top = stack.isEmpty() ? '#' : stack.pop();
+            if (')' == value &&'(' != top) {
+                return false;
+            }
+            if ('}' == value  && '{'!= top) {
+                return false;
+            }
+            if (']' == value && '[' != top) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+    public static void main(String[] args) {
+        Subject_20 demo = new Subject_20();
+        System.out.println(demo.isValid("){"));
+    }
+}
